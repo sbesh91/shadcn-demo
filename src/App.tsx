06286cddc72c3de0d7f65ab1b2b8c1e081c84c73
Steven Beshensky <s.beshensky@gmail.com>
2023-06-09 16:22:33 -0500

    experimenting with composing command and popover

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Check, ChevronsUpDown } from "lucide-react";
 import { useState } from "react";
 import { Button } from "~/ui/button";
 import { Checkbox } from "~/ui/checkbox";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "~/ui/command";
 import {
   Dialog,
   DialogContent,
@@ -12,6 +20,31 @@ import {
 } from "~/ui/dialog";
 import { Input } from "~/ui/input";
 import { Label } from "~/ui/label";
+import { Popover, PopoverContent, PopoverTrigger } from "~/ui/popover";
+import { cn } from "./lib/utils";
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+];
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -68,6 +101,12 @@ function App() {
                   className="col-span-3"
                 />
               </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label className="text-right">Combobox</Label>
+                <div className="col-span-3">
+                  <Combobox />
+                </div>
+              </div>
             </div>
             <DialogFooter>
               <Button type="submit">Save changes</Button>
@@ -76,6 +115,55 @@ function App() {
         </Dialog>
       </div>
     </div>
+  );
+}
+
+function Combobox() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? frameworks.find((framework) => framework.value === value)?.label
+            : "Select framework..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search framework..." />
+          <CommandEmpty>No framework found.</CommandEmpty>
+          <CommandGroup>
+            {frameworks.map((framework) => (
+              <CommandItem
+                className="cursor-pointer"
+                key={framework.value}
+                onSelect={(currentValue) => {
+                  setValue(currentValue === value ? "" : currentValue);
+                  setOpen(false);
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    value === framework.value ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {framework.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
   );
 }
 

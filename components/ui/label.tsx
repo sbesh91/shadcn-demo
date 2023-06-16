d2c7d5c2a2399a6b2f48f9c16552ade610ae4e4b
Steven Beshensky <s.beshensky@gmail.com>
2023-06-16 13:00:26 -0500

    testing hover interactions

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -5,12 +5,13 @@ import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import {motion} from "framer-motion";
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
-const Label = React.forwardRef<
+const Label = motion(React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
     VariantProps<typeof labelVariants>
@@ -20,7 +21,7 @@ const Label = React.forwardRef<
     className={cn(labelVariants(), className)}
     {...props}
   />
-))
+)))
 Label.displayName = LabelPrimitive.Root.displayName
 
 export { Label }
